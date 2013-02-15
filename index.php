a3f46b2c1270873e5c61f98bd680b1147c0b3070
Igor Dulger <igordulger@gmail.com>
2013-02-15 11:11:24 +0200

    rselect function

--- index.php
+++ index.php
@@ -5,20 +5,20 @@ date_default_timezone_set('Europe/Lisbon');
 
 $data = array();
 
-$data_temp = file("QuickSort.txt");
-foreach($data_temp as $el) {
-    $data[] = (int)$el;
-} 
-
-//$data = array(2, 8, 9, 3, 7, 5, 10, 1, 6, 4);
-//$data = array();
-//for ($i=1;$i<=100;$i++){
-////for ($i=100;$i>=1;$i--){
-//    $data[] = $i;
+//$data_temp = file("QuickSort.txt");
+//foreach($data_temp as $el) {
+//    $data[] = (int)$el;
 //}
-//shuffle($data);
 
-include "quick_sort.php";
+$data = array(2, 8, 9, 3, 7, 5, 10, 1, 6, 4);
+$data = array();
+for ($i=1;$i<=100;$i++){
+//for ($i=100;$i>=1;$i--){
+    $data[] = $i;
+}
+shuffle($data);
+
+include "select.php";
 //include "merge_sort.php";
 /*
  * To change this template, choose Tools | Templates
---
+++ select.php
@@ -0,0 +1,49 @@
+<?php
+$work = 0;
+function swap(&$data, $a, $b){
+    $t = $data[$a];
+    $data[$a] = $data[$b];
+    $data[$b] = $t;
+}
+function partitionate(&$data, $left, $right){
+    global $work;
+    $work += ($right - $left) - 1;
+    $pivit = mt_rand($left, $right-1);
+    swap($data, $left, $pivit);
+
+    $pivit = $left;
+    $i = $left+1;
+    for ($j=$left+1; $j<$right; $j++){
+        if ($data[$pivit] > $data[$j] ) {
+            swap($data, $i, $j);
+            $i++;
+        }
+    }
+    swap($data, $pivit, $i-1);
+    return $i-1-$left;
+}
+
+function rselect(&$data, $left, $right, $target){
+    global $work;
+
+//print_r(array("left" => $left, "right" => $right, "target" => $target, "data" => $data));
+
+    if ($right == $left+1) return $data[$left];
+
+    $pivit = partitionate($data, $left, $right);
+
+    if ($pivit == $target) return $data[$left+$pivit];
+
+    if ($pivit > $target) {
+        return rselect($data, $left, $left+$pivit, $target);
+    }
+
+    if ($pivit < $target) {
+        return rselect($data, $left+$pivit+1, $right, $target-$pivit-1);
+    }
+}
+
+$res = rselect($data, 0, count($data), 44);
+print_r(array($res, $work));
+?>
+
